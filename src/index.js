7551ff78a42817b47be71c3e28a7a21279632171
manel turki <menelturki@gmail.com>
2020-02-25 20:23:34 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-let addToy = false;
+function getResponse(){
+  let configObj = {
+    method: "GET",
+    headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+    },
+    body: ''
+  };
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyForm = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyForm.style.display = "block";
-    } else {
-      toyForm.style.display = "none";
-    }
-  });
-});
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      let div = document.getElementById('toy-collection');
+      let div1 = document.createElement('div');
+      let h = document.createElement(h2);
+      let img = document.createElement(img);
+      let p= document.createElement('p');
+      let btn = document.createElement('button');
+      divT.className = 'card';
+      btn.classList.add("like-btn");
+       for(toy of object)
+      {
+         h.innerHTML = toy.name;          
+         img.src = toy.image ;          
+         p.innerHTML= toy.likes;          
+         div1.prepend(h);
+         h.after(img);
+         img.after(p);
+         p.after(btn);
+         div.appendChild(divT);         
+        }
+    });
+
+}
+getResponse();
